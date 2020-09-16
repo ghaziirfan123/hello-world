f86bdeda1346de6b3dbaf35d330acff526072f6c
ghaziirfan123 <70496821+ghaziirfan123@users.noreply.github.com>
2020-09-16 11:48:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # My name is Mohammad Irfan and my peoplesoft is 1626488
-user_num=int(input("Enter integer: "))
-squared=user_num*user_num
-cubed=squared*user_num
-print("You entered: ",user_num)
-print(user_num," squared is ",squared)
-print("And ",user_num," cubed is ",cubed," !!")
-user_num2=int(input("Enter another integer: "))
-print(user_num," + ",user_num2," is ",user_num+user_num2)
-print(user_num," * ",user_num2," is ",user_num*user_num2)
+userNum = input()
+if userNum == '':
+    userNum = 0
+userNumSquared = int(userNum) * int(userNum)
+print(userNumSquared)
