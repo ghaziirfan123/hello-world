94a5860000d63238efa7e5f440717cc4812a8420
ghaziirfan123 <70496821+ghaziirfan123@users.noreply.github.com>
2020-09-16 11:52:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-# My name is Mohammad Irfan and my peoplesoft is 1626488
-user_num=int(input("Enter integer: "))
-squared=user_num*user_num
-cubed=squared*user_num
-print("You entered: ",user_num)
-print(user_num," squared is ",squared)
-print("And ",user_num," cubed is ",cubed," !!")
-user_num2=int(input("Enter another integer: "))
-print(user_num," + ",user_num2," is ",user_num+user_num2)
-print(user_num," * ",user_num2," is ",user_num*user_num2)
+# my name is mohammad irfan and my peoplesoft is 1626488
+service_schedule = {"-": 0, "Oil change": 35, "Tire rotation": 19, "Car wash": 7, "Car wax": 12}
+print("Davy's auto shop services")
+print("Oil change -- $35")
+print("Tire rotation -- $19")
+print("Car wash -- $7")
+print("Car wax -- $12")
+print("")
+first_service = input("Select first service:")
+print("")
+second_service = input("Select second service:")
+print("")
+print("Davy's auto shop invoice")
+print("")
+if (first_service == "-"):
+    print("Service 1: No service")
+else:
+    print("Service 1: {:s}, ${:d}".format (first_service, service_schedule.get(first_service)))
+if (second_service == "-"):
+    print("Service 2: No service")
+else:
+    print("Service 2: {:s}, ${:d}".format (second_service, service_schedule.get(second_service)))
+invoice_total = service_schedule.get(first_service) + service_schedule.get(second_service)
+print("Total: ${:d}".format(invoice_total))
